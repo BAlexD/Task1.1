ab7e3258080f682f2cefe6a5abb376389df0b802
Алексей Болдырев <55403749+BAlexD@users.noreply.github.com>
2019-09-25 22:31:35 +0300

    Create Task1_1.ino

---
+++ Task1_1/Task1_1.ino
@@ -0,0 +1,30 @@
+int button = 0;
+void setup() {
+  pinMode(2, OUTPUT);//red
+  pinMode(3, OUTPUT);//yellow
+  pinMode(4, OUTPUT);//green
+  pinMode(5, INPUT);//button 
+  
+
+}
+
+void loop() {
+  button = digitalRead(5);
+  if (button == HIGH){
+   digitalWrite(2,HIGH);
+   delay (1000);
+   digitalWrite(2,LOW);
+   delay (1000);
+   digitalWrite(3,HIGH);
+   delay (1000);
+   digitalWrite(3,LOW);
+   delay (1000);
+   digitalWrite(4,HIGH);
+   delay (1000);
+   digitalWrite(4,LOW);
+   delay (1000);
+  }
+  else{
+    digitalWrite(2,HIGH);
+  }
+}
